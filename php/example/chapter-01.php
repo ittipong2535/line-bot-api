fc8839885318a05c2f7f94eb2d5fc67a9f98d49a
ittipong2535 <33741925+ittipong2535@users.noreply.github.com>
2017-11-18 19:32:06 +0700

    Update chapter-01.php

--- php/example/chapter-01.php
+++ php/example/chapter-01.php
@@ -2,8 +2,8 @@
 
 include ('line-bot-api/php/line-bot.php');
 
-$channelSecret = '551ec4feee0.....43cff0';
-$access_token  = '2og9ogezC8k.......5ZUEQQdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '12ac32778389db4066a4237fb66a247d';
+$access_token  = 'CbTv7VdQFapQHopr9CHcZwsgc+mLHhyJWsZ2D6kQUED72fUlpEM8fwmgSsCr1i1groolVNS9MPcbq0/0STdJfQsSDm7eog9tclyge8zBxPLhudxUt5pl5eUARamJxlvMouf8qjCI64dhYEYBEqSvxAdB04t89/1O/w1cDnyilFU=';
 
 $bot = new BOT_API($channelSecret, $access_token);
 	
